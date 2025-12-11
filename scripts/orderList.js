7dbd6f7129ef93469224f7028cdc7f94d1a6621a
cmdrumright <cdrumright@protonmail.com>
2025-12-11 15:13:19 -0600

    - udpated order fetch url to expand all foreign keys - added formula to calculate total order price - converted total price to locale string and added to order card

--- scripts/orderList.js
+++ scripts/orderList.js
@@ -1,11 +1,17 @@
 export const orderList = async () => {
-    const response = await fetch("http://localhost:8088/orders")
+    const response = await fetch("http://localhost:8088/orders?_expand=wheel&_expand=paint&_expand=interior&_expand=dashboard")
     const orders = await response.json()
 
     let ordersHTML = orders.map((order) => {
+        const price = order.wheel.price + order.paint.price + order.interior.price + order.dashboard.price
+        const localPrice = price.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD"
+        })
+
         return `
             <div class="order-card">
-                Order #${order.id}
+                Order #${order.id} price: ${localPrice}
             </div>
         `
     }).join("")
